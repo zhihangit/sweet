c64883f6d00e316537b5755505a77f893f6adbd5
lzh <38020076@qq.com>
2020-06-27 11:27:59 +0800

    用户端首页

--- public/index.php
+++ public/index.php
@@ -1,5 +1,6 @@
 <?php
-// +----------------------------------------------------------------------
+
+ /// +----------------------------------------------------------------------
 // | ThinkPHP [ WE CAN DO IT JUST THINK ]
 // +----------------------------------------------------------------------
 // | Copyright (c) 2006-2018 http://thinkphp.cn All rights reserved.
@@ -19,3 +20,6 @@ require __DIR__ . '/../thinkphp/base.php';
 
 // 执行应用并响应
 Container::get('app')->run()->send();
+// 绑定当前访问到index模块的index控制器
+//Container::get('app')->bind('index/index')->run()->send();
+
