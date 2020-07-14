d7bf2a71253e9c60e66b95349d1a91a283a908a8
zhihangit <38020076@qq.com>
2020-07-14 08:49:24 +0800

    随机生成码

--- application/common.php
+++ application/common.php
@@ -8,5 +8,20 @@
 // +----------------------------------------------------------------------
 // | Author: 流年 <liu21st@gmail.com>
 // +----------------------------------------------------------------------
-
+function getrandstr1($length){
+    $str='abcdefghijklmnopqrstuvwxyz0123456789';
+    $len=strlen($str)-1;
+    $randstr='';
+    for($i=0;$i<$length;$i++){
+        $num=mt_rand(0,$len);
+        $randstr .= $str[$num];
+    }
+    return $randstr;
+}
+function getrandstr2(){
+    $str='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890';
+    $randStr = str_shuffle($str);//打乱字符串
+    $rands= substr($randStr,0,4);//substr(string,start,length);返回字符串的一部分
+    return $rands;
+}
 // 应用公共文件
